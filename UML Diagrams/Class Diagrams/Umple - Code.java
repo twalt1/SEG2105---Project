ad927f4f3976c045589b4ac36d1a08beceed95e3
twalt1 <31697661+twalt1@users.noreply.github.com>
2021-10-17 13:24:34 -0400

    Diagram Folders added

---
+++ UML Diagrams/Class Diagrams/Umple - Code.java
@@ -0,0 +1,55 @@
+class User
+{
+  string userName;
+  string passWord;
+  int acctRank;
+  public void createAcct(){
+    
+  }
+}
+
+class Admin
+{
+  isA User;
+  public void createClass(){
+    
+  }
+  public void editClass(){
+    
+  }
+  public void removeClass(){
+    
+  }
+  public void deleteAcct(){
+    
+  }
+}
+
+class Instructors
+{
+  isA User;
+  public void instructClass(){
+    
+  }
+  public void editClass(){
+    
+  }
+  public void cancelClass(){
+    
+  }
+}
+
+class Members
+{
+  isA User;
+  public void viewClass(){
+    
+  }
+  public void searchClass(){
+    
+  }
+  public void enrollClass(){
+    
+  }
+}
+
