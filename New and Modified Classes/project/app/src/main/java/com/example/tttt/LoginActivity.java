6344269b2e674045a16ce0cc48844d33ace99a6e
EkkoLizehan <64797122+EkkoLizehan@users.noreply.github.com>
2021-10-19 20:50:48 -0400

    Update LoginActivity.java

--- New and Modified Classes/project/app/src/main/java/com/example/tttt/LoginActivity.java
+++ New and Modified Classes/project/app/src/main/java/com/example/tttt/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.tttt;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +18,15 @@ public class LoginActivity extends AppCompatActivity {
     ImageButton btn1;
     Button btn2, btn3;
     TextInputEditText input1, input2;
+    DBHelper1 db1;
+    DBHelper2 db2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db1 = new DBHelper1(this);
+        db2 = new DBHelper2(this);
 
         btn1 = findViewById(R.id.back_button_l);
         btn3 = findViewById(R.id.login_l);
@@ -43,6 +51,61 @@ public class LoginActivity extends AppCompatActivity {
 
                 Intent i=new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(i);
+
+            }
+
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+                try{
+                    String enteredUsername = input1.getText().toString();
+                    String enteredPassword = input2.getText().toString();
+                    Boolean flag1 = db1.checkusernamepassword(enteredUsername, enteredPassword);
+                    Boolean flag2 = db2.checkusernamepassword(enteredUsername, enteredPassword);
+                    if (!flag1){
+
+                        if (flag2) {
+
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+
+                            String aString = enteredUsername + " Instructor";
+
+                            Intent intent = new Intent(getApplicationContext(), Welcome.class);
+                            intent.putExtra("name", aString);
+                            startActivity(intent);
+
+                        }   else {
+
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    } else {
+
+                        Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                        //start new welcome page
+
+                        String aString = enteredUsername + " Gymmember";
+
+                        Intent intent = new Intent(getApplicationContext(), Welcome.class);
+                        intent.putExtra("name", aString);
+                        startActivity(intent);
+
+                    }
+
+                }
+
+                catch(Exception e){
+
+                    Toast.makeText(LoginActivity.this, "ERROR: " + e.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+
+
 
             }
 
