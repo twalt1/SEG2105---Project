7d33c20b4fe220326f73639b8314d9516d6513a0
twalt1 <31697661+twalt1@users.noreply.github.com>
2021-10-20 00:33:59 -0400

    Add Class Files

---
+++ New and Modified Classes/project/app/src/main/java/com/example/tttt/DBHelper3.java
@@ -0,0 +1,71 @@
+package com.example.tttt;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class DBHelper3 extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Class.db";
+    public static final String TABLE_NAME = "User_table";
+    public static final String COL_1 = "Title";
+    public static final String COL_2 = "Description";
+    public static final String COL_3 = "Difficulty";
+    public static final String COL_4 = "Capacity";
+    public static final String COL_5 = "Date";
+    public static final String COL_6 = "Time";
+    //public static final String COL_7 = "Status";
+
+    public DBHelper3(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT," +" TITLE TEXT, DESCRIPTION TEXT, DIFFICULTY TEXT, CAPACITY INTEGER, DATE TEXT, TIME TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+
+    public boolean insertData(String title, String description, String difficulty, String capacity, String date, String time) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, title);
+        contentValues.put(COL_2, description);
+        contentValues.put(COL_3, difficulty);
+        contentValues.put(COL_4, capacity);
+        contentValues.put(COL_5, date);
+        contentValues.put(COL_6, time);
+
+        long res = db.insert(TABLE_NAME, null, contentValues);
+
+        if(res == -1) {
+
+            return false;
+
+        }   else {
+
+            return true;
+
+        }
+
+
+    }
+
+}
