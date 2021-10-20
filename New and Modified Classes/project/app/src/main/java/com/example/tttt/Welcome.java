58dd13f0a403ea5590fb7a41cf89f7cf97c6ce4a
EkkoLizehan <64797122+EkkoLizehan@users.noreply.github.com>
2021-10-20 00:50:52 -0400

    Update Welcome.java

--- New and Modified Classes/project/app/src/main/java/com/example/tttt/Welcome.java
+++ New and Modified Classes/project/app/src/main/java/com/example/tttt/Welcome.java
@@ -3,6 +3,7 @@ package com.example.tttt;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Welcome extends AppCompatActivity {
 
     TextView display;
+    public static final int SPLASH_SCREEN = 2000;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,6 +26,33 @@ public class Welcome extends AppCompatActivity {
         String got[] = intent.getStringExtra("name").split(" ");
 
         display.setText("Welcome\n" + got[0] + "!\n\n You are logged in as\n\n" +got[1]);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (got[1].equals("GymMember")) {
+
+                    Intent intent = new Intent(getApplicationContext(), ClassActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }   else if (got[1].equals("Instructor")) {
+
+                    Intent intent = new Intent(getApplicationContext(), ClassActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }   else if (got[1].equals("Admin")) {
+
+                    Intent intent = new Intent(getApplicationContext(), AdminActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+            }
+        } ,SPLASH_SCREEN);
 
     }
 
