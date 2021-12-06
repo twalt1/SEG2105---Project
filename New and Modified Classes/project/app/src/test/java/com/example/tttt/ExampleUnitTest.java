6aa18ed49e64e8fa8ba0d76a89a5887b13ccd148
EkkoLizehan <64797122+EkkoLizehan@users.noreply.github.com>
2021-12-05 19:27:12 -0500

    Update ExampleUnitTest.java

--- New and Modified Classes/project/app/src/test/java/com/example/tttt/ExampleUnitTest.java
+++ New and Modified Classes/project/app/src/test/java/com/example/tttt/ExampleUnitTest.java
@@ -14,20 +14,4 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-
-
-    @Test
-    public void getStringAfterUnenroll_Test(){
-
-        //remove first member
-        String s = "gym1@gmail.com, gym2@gmail.com, gym3@gmail.com";
-        assertEquals("gym2@gmail.com, gym3@gmail.com", DBClass.getStringAfterUnenroll(s, "gym1@gmail.com"));
-        //remove second member
-        s = "gym1@gmail.com, gym2@gmail.com, gym3@gmail.com";
-        assertEquals("gym1@gmail.com, gym3@gmail.com", DBClass.getStringAfterUnenroll(s, "gym2@gmail.com"));
-        //remove last member
-        s = "gym1@gmail.com, gym2@gmail.com, gym3@gmail.com";
-        assertEquals("gym1@gmail.com, gym2@gmail.com", DBClass.getStringAfterUnenroll(s, "gym3@gmail.com"));
-    }
 }
